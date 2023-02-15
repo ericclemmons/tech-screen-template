5dc6c5fe612eb706ee9f0017ef892ed1503b0f1c
Eric Clemmons <eric@smarterspam.com>
2023-02-15 11:34:23 -0600

    pnpm astro add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
@@ -7,6 +7,9 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
+  integrations: [mdx(), react(), tailwind()],
 });
